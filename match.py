e272c1a4d03c83d1e32d93d46be6d9e60cc1e0b6
xzqt23 <angelo.agualada@gmail.com>
2023-03-01 19:54:33 +0800

    Create match.py

---
+++ match.py
@@ -0,0 +1,14 @@
+http_status = 200
+
+match http_status:
+    case 200 | 201:
+        print("Success!")
+
+    case 400:
+        print("Not Found")
+    
+    case 500 | 501:
+        print("Server Error")
+    
+    case _:
+        print("Unknown")
